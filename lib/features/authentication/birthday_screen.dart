d79f5911ec80c7bf4f876c5cd2801e2750cbbb19
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-04-09 20:55:15 +0900

    create birthday screen

--- lib/features/authentication/birthday_screen.dart
+++ lib/features/authentication/birthday_screen.dart
@@ -1,10 +1,119 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
-class BirthdayScreen extends StatelessWidget {
+class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({super.key});
 
   @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+
+  final DateTime _initialDate = DateTime(
+    DateTime.now().year - 12,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _setTextFieldDate(_initialDate);
+  }
+
+  @override
+  void dispose() {
+    _birthdayController.dispose();
+    super.dispose();
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = DateFormat('y-MM-dd').format(date);
+    _birthdayController.value = TextEditingValue(text: textDate);
+  }
+
+  void _onFormButtonTap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "When's your birthday",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              "Your birthday won't be shown publicy",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black45,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              enabled: false,
+              controller: _birthdayController,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v16,
+            FormButton(
+              disabled: false,
+              onTap: _onFormButtonTap,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 300,
+          child: CupertinoDatePicker(
+            maximumDate: _initialDate,
+            initialDateTime: _initialDate,
+            mode: CupertinoDatePickerMode.date,
+            onDateTimeChanged: (value) => _setTextFieldDate(value),
+          ),
+        ),
+      ),
+    );
   }
 }
