be6af1331ed3d46deb6989487a47a3caea9d4430
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-04-09 12:39:24 +0900

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:tiktok_clone/generated/l10n.dart';
-import 'package:tiktok_clone/router.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await SystemChrome.setPreferredOrientations(
-    [
-      DeviceOrientation.portraitUp,
-    ],
-  );
-
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle.dark,
-  );
-
-  runApp(const TikTokApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class TikTokApp extends StatelessWidget {
-  const TikTokApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // S.load(const Locale('en'));
-    Color primaryColor = const Color(0xffe9435a);
-
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-      title: 'TikTok Clone',
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('ko'),
-      ],
-      themeMode: ThemeMode.system,
+    return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        textTheme: Typography.blackMountainView,
-        tabBarTheme: TabBarTheme(
-          labelColor: Colors.black,
-          unselectedLabelColor: Colors.grey.shade500,
-          indicatorColor: Colors.black,
-        ),
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: primaryColor,
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: primaryColor,
-        ),
-        splashColor: Colors.transparent,
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: 16 + 2,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        listTileTheme: const ListTileThemeData(iconColor: Colors.black),
+        primaryColor: Color(0xffe9435a),
       ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        textTheme: Typography.whiteMountainView,
-        tabBarTheme: TabBarTheme(
-          indicatorColor: Colors.white,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.grey.shade700,
-        ),
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: primaryColor,
-        ),
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: AppBarTheme(
-          surfaceTintColor: Colors.grey.shade900,
-          backgroundColor: Colors.grey.shade900,
-          foregroundColor: Colors.white,
-          titleTextStyle: const TextStyle(
-            fontSize: 16 + 2,
-            fontWeight: FontWeight.w600,
-            color: Colors.white,
-          ),
-          actionsIconTheme: IconThemeData(
-            color: Colors.grey.shade100,
-          ),
-        ),
-        bottomAppBarTheme: BottomAppBarTheme(
-          color: Colors.grey.shade900,
-        ),
-        primaryColor: primaryColor,
-      ),
+      home: const Scaffold(),
     );
-    // return const CupertinoApp(
-    //   title: 'TikTok Clone',
-    //   home: MainNavigationScreen(),
-    // );
   }
 }
