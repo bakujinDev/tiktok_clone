374d35f1c6d99c229599aadcdaddba15afc38a1d
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-04-10 18:38:12 +0900

    create off stage

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/main_navigation/stf_screen.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -10,15 +14,6 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    const Center(
-      child: Text("Home"),
-    ),
-    const Center(
-      child: Text("Search"),
-    ),
-  ];
-
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
@@ -27,20 +22,72 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.house),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.search),
-            label: "Search",
-          ),
-        ],
+    var screens = [
+      Offstage(
+        offstage: _selectedIndex != 0,
+        child: const StfScreen(),
       ),
-      tabBuilder: (context, index) => screens[index],
+      Offstage(
+        offstage: _selectedIndex != 1,
+        child: const StfScreen(),
+      ),
+      Offstage(
+        offstage: _selectedIndex != 2,
+        child: const StfScreen(),
+      ),
+      Offstage(
+        offstage: _selectedIndex != 3,
+        child: const StfScreen(),
+      ),
+      Offstage(
+        offstage: _selectedIndex != 4,
+        child: const StfScreen(),
+      ),
+    ];
+
+    return Scaffold(
+      body: Stack(
+        children: screens,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                icon: FontAwesomeIcons.house,
+                selectedicon: FontAwesomeIcons.house,
+                text: "House",
+                isSelected: _selectedIndex == 0,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.compass,
+                selectedicon: FontAwesomeIcons.solidCompass,
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.message,
+                selectedicon: FontAwesomeIcons.solidMessage,
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.user,
+                selectedicon: FontAwesomeIcons.solidUser,
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  const NavTab({
+    super.key,
+    required this.icon,
+    required this.selectedicon,
+    required this.text,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final IconData selectedicon;
+  final String text;
+  final bool isSelected;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          color: Colors.black,
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 100),
+            opacity: isSelected ? 1 : 0.6,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  isSelected ? selectedicon : icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
