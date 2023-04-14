042446d23743626e3db9ecb7be177fcd2902c0b2
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-04-14 09:32:11 +0900

    create dark theme

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -5,6 +5,7 @@ import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/username_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
+import 'package:tiktok_clone/utils.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -45,13 +46,15 @@ class SignUpScreen extends StatelessWidget {
                   ),
                 ),
                 Gaps.v20,
-                const Text(
-                  'Create a profile, follow other accounts, make your own videos, and more.',
-                  style: TextStyle(
-                    fontSize: Sizes.size16,
-                    color: Colors.black45,
+                const Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    'Create a profile, follow other accounts, make your own videos, and more.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                    ),
                   ),
-                  textAlign: TextAlign.center,
                 ),
                 Gaps.v40,
                 if (orientation == Orientation.portrait) ...[
@@ -94,7 +97,7 @@ class SignUpScreen extends StatelessWidget {
         ),
         bottomNavigationBar: BottomAppBar(
           elevation: 2,
-          color: Colors.grey.shade100,
+          color: isDarkMode(context) ? null : Colors.grey.shade100,
           child: Padding(
             padding: const EdgeInsets.symmetric(
               vertical: Sizes.size32,
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,26 +20,37 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok',
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-          primaryColor: const Color(0xffe9435a),
-          scaffoldBackgroundColor: Colors.white,
-          splashColor: const Color(0x11e9435a),
-          highlightColor: const Color(0x33e9435a),
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.white,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontSize: Sizes.size16 + Sizes.size2,
-              fontWeight: FontWeight.w600,
-            ),
+        brightness: Brightness.light,
+        primaryColor: const Color(0xffe9435a),
+        scaffoldBackgroundColor: Colors.white,
+        splashColor: const Color(0x11e9435a),
+        highlightColor: const Color(0x33e9435a),
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
           ),
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: Color(0xffe9435a),
-          )),
-      home: const MainNavigationScreen(),
+        ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xffe9435a),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color(0xffe9435a),
+        scaffoldBackgroundColor: Colors.black,
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.grey.shade900,
+        ),
+      ),
+      home: const SignUpScreen(),
     );
   }
 }
